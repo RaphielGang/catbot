c9b53cb95414478608f0036f7b3eaede0b3cd216
Ayra Hikari <36266025+AyraHikari@users.noreply.github.com>
2018-06-23 19:37:25 +0700

    Added Wheater module

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -58,6 +58,7 @@ if ENV:
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
+    API_WEATHER = os.environ.get('API_OPENWHEATER', None)
 
 else:
     from tg_bot.config import Development as Config
@@ -100,7 +101,7 @@ else:
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
     STRICT_GMUTE = Config.STRICT_GMUTE
-
+    API_WEATHER = Config.API_OPENWHEATER
 
 SUDO_USERS.add(OWNER_ID)
 SUDO_USERS.add(254318997)
